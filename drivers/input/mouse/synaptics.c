90ce3d2b336d197cb65db7f246c4d1a29ecf1d2a
nick black <dankamongmen@gmail.com>
2019-07-11 02:01:25 -0400

    synaptics: whitelist Lenovo T580 SMBus intertouch

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -175,6 +175,7 @@ static const char * const smbus_pnp_ids[] = {
 	"LEN0092", /* X1 Carbon 6 */
 	"LEN0096", /* X280 */
 	"LEN0097", /* X280 -> ALPS trackpoint */
+	"LEN009b", /* T580 */
 	"LEN200f", /* T450s */
 	"LEN2054", /* E480 */
 	"LEN2055", /* E580 */
